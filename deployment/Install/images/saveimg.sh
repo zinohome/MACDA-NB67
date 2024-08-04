97cbf0e66d41c0f2fd7a84dce35bad488cd0cb56
zhangjun <ibmzhangjun@139.com>
2024-08-04 10:39:54 +0800

    Init

--- deployment/Install/images/saveimg.sh
+++ deployment/Install/images/saveimg.sh
@@ -1,22 +1,22 @@
-docker save -o redpanda.tar harbor.zinohome.com/macda/redpanda:v22.3.9 && \
+docker save -o redpanda.tar harbor.naivehero.top/macda/redpanda:v22.3.9 && \
 gzip redpanda.tar && \
-docker save -o redpanda-console.tar harbor.zinohome.com/macda/redpanda-console:v2.1.1 && \
+docker save -o redpanda-console.tar harbor.naivehero.top/macda/redpanda-console:v2.1.1 && \
 gzip redpanda-console.tar && \
-docker save -o kafka-connect.tar harbor.zinohome.com/macda/kafka-connect:latest && \
+docker save -o kafka-connect.tar harbor.naivehero.top/macda/kafka-connect:latest && \
 gzip kafka-connect.tar && \
 docker save -o passenger.tar phusion/passenger-full:latest && \
 gzip passenger.tar && \
-docker save -o kafka-ui.tar harbor.zinohome.com/macda/kafka-ui:latest && \
+docker save -o kafka-ui.tar harbor.naivehero.top/macda/kafka-ui:latest && \
 gzip kafka-ui.tar && \
-docker save -o ksqldb.tar harbor.zinohome.com/macda/ksqldb-server:0.27.2 && \
+docker save -o ksqldb.tar harbor.naivehero.top/macda/ksqldb-server:0.27.2 && \
 gzip ksqldb.tar && \
-docker save -o ksqldb-cli.tar harbor.zinohome.com/macda/ksqldb-cli:0.27.2 && \
+docker save -o ksqldb-cli.tar harbor.naivehero.top/macda/ksqldb-cli:0.27.2 && \
 gzip ksqldb-cli.tar && \
-docker save -o mirror-maker.tar harbor.zinohome.com/macda/mirror-maker2:2.8.1 && \
+docker save -o mirror-maker.tar harbor.naivehero.top/macda/mirror-maker2:2.8.1 && \
 gzip mirror-maker.tar && \
-docker save -o minio.tar harbor.zinohome.com/macda/minio:latest && \
+docker save -o minio.tar harbor.naivehero.top/macda/minio:latest && \
 gzip minio.tar && \
-docker save -o mc.tar harbor.zinohome.com/macda/mc:latest && \
+docker save -o mc.tar harbor.naivehero.top/macda/mc:latest && \
 gzip mc.tar && \
 docker save -o timescaledb.tar timescale/timescaledb-ha:pg14-latest && \
 gzip timescaledb.tar && \
@@ -24,6 +24,6 @@ docker save -o pgadmin4.tar dpage/pgadmin4:latest && \
 gzip pgadmin4.tar && \
 docker save -o macda-nb.tar jointhero/macda:nb-v1.0911 && \
 gzip macda-nb.tar && \
-docker save -o desktop.tar harbor.zinohome.com/kasmweb/kasm-desktop-focal-web:1.12.1 && \
+docker save -o desktop.tar harbor.naivehero.top/kasmweb/kasm-desktop-focal-web:1.12.1 && \
 gzip desktop.tar
 
