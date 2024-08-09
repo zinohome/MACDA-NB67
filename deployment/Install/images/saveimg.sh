92094c85c877e3f9945b595ec73a914fcd9ec7d2
zhangjun <ibmzhangjun@139.com>
2024-08-09 21:15:45 +0800

    Init

--- deployment/Install/images/saveimg.sh
+++ deployment/Install/images/saveimg.sh
@@ -1,6 +1,6 @@
-docker save -o redpanda.tar harbor.naivehero.top/macda/redpanda:v22.3.9 && \
+docker save -o redpanda.tar harbor.naivehero.top/macda/redpanda:v24.2.1 && \
 gzip redpanda.tar && \
-docker save -o redpanda-console.tar harbor.naivehero.top/macda/redpanda-console:v2.1.1 && \
+docker save -o redpanda-console.tar harbor.naivehero.top/macda/redpanda-console:v2.7.0 && \
 gzip redpanda-console.tar && \
 docker save -o kafka-connect.tar harbor.naivehero.top/macda/kafka-connect:latest && \
 gzip kafka-connect.tar && \
@@ -8,9 +8,9 @@ docker save -o passenger.tar phusion/passenger-full:latest && \
 gzip passenger.tar && \
 docker save -o kafka-ui.tar harbor.naivehero.top/macda/kafka-ui:latest && \
 gzip kafka-ui.tar && \
-docker save -o ksqldb.tar harbor.naivehero.top/macda/ksqldb-server:0.27.2 && \
+docker save -o ksqldb.tar harbor.naivehero.top/macda/ksqldb-server:0.29.0 && \
 gzip ksqldb.tar && \
-docker save -o ksqldb-cli.tar harbor.naivehero.top/macda/ksqldb-cli:0.27.2 && \
+docker save -o ksqldb-cli.tar harbor.naivehero.top/macda/ksqldb-cli:0.29.0 && \
 gzip ksqldb-cli.tar && \
 docker save -o mirror-maker.tar harbor.naivehero.top/macda/mirror-maker2:2.8.1 && \
 gzip mirror-maker.tar && \
@@ -20,7 +20,9 @@ docker save -o mc.tar harbor.naivehero.top/macda/mc:latest && \
 gzip mc.tar && \
 docker save -o timescaledb.tar timescale/timescaledb-ha:pg14-latest && \
 gzip timescaledb.tar && \
-docker save -o pgadmin4.tar dpage/pgadmin4:latest && \
+docker save -o graphql.tar harbor.naivehero.top/macda/graphql-engine:v2.40.0 && \
+gzip graphql.tar && \
+docker save -o harbor.naivehero.top/macda/pgadmin4:8.10 && \
 gzip pgadmin4.tar && \
 docker save -o macda-nb.tar jointhero/macda:nb-v1.2408 && \
 gzip macda-nb.tar && \
