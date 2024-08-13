aec924c775af7f6621e8e0e17f07c3dfcd7f45c2
zhangjun <ibmzhangjun@139.com>
2024-08-13 17:26:58 +0800

    Init

---
+++ deployment/Install/images/pullimg.sh
@@ -0,0 +1,15 @@
+docker pull harbor.naivehero.top/macda/redpanda:v24.2.1 && \
+docker pull harbor.naivehero.top/macda/redpanda-console:v2.7.0 && \
+docker pull harbor.naivehero.top/macda/kafka-connect:latest && \
+docker pull harbor.naivehero.top/baseimages/passenger-full:latest && \
+docker pull harbor.naivehero.top/macda/kafka-ui:latest && \
+docker pull harbor.naivehero.top/macda/ksqldb-server:0.29.0 && \
+docker pull harbor.naivehero.top/macda/ksqldb-cli:0.29.0 && \
+docker pull harbor.naivehero.top/macda/mirror-maker2:2.8.1 && \
+docker pull harbor.naivehero.top/macda/minio:latest && \
+docker pull harbor.naivehero.top/macda/mc:latest && \
+docker pull harbor.naivehero.top/macda/timescaledb-ha:pg14-latest && \
+docker pull harbor.naivehero.top/macda/graphql-engine:v2.40.0 && \
+docker pull harbor.naivehero.top/macda/pgadmin4:8.10 && \
+docker pull harbor.naivehero.top/macda/macda:nb67-v1.2408 && \
+docker pull harbor.naivehero.top/kasmweb/kasm-desktop-focal-web:1.12.1
